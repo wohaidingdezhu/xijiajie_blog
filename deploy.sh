dd2cc5aa05b9c735f72ddbdefbc72afd05e98b83
xijiajie <1054530015@qq.com>
2021-12-02 00:39:13 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+# cd public
+
+# git init
+git add .
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
+git push -f git@github.com:wohaidingdezhu/wohaidingdezhu.github.io.git master
+
+cd ..
+
+
