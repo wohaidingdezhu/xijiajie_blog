a0fd70f3e29368c1a6ba2ae4e4ab1057595a7953
xijiajie <1054530015@qq.com>
2021-12-22 23:47:37 +0800

    新增Promise

--- .vuepress/siderbar.js
+++ .vuepress/siderbar.js
@@ -1,6 +1,7 @@
 const { createSideBarConfig } = require("./util");
 const JAVASCRIPT_PATH = "/blogs/javascript";
 const WEBPACK_PATH = "/blogs/webpack";
+const GIT_PATH = "/blogs/git";
 
 module.exports = {
   [JAVASCRIPT_PATH]: [
@@ -8,4 +9,5 @@ module.exports = {
     createSideBarConfig("JS-进阶", JAVASCRIPT_PATH + "/js-advanced"),
   ],
   [WEBPACK_PATH]: [createSideBarConfig("webpack相关", WEBPACK_PATH)],
+  [GIT_PATH]: [createSideBarConfig("Git使用", GIT_PATH + '/git-base')],
 };
